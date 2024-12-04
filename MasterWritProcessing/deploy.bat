35f8464aac5f74cf42560c5407b461312eebb1e0
Alex Vasilenko <vasilenko.a@cashberry.vn>
2024-12-04 18:54:39 +0200

    ~ Master Writ Processing

--- MasterWritProcessing/deploy.bat
+++ MasterWritProcessing/deploy.bat
@@ -2,4 +2,6 @@ rmdir /s /q c:\Users\ventr\Documents\"Elder Scrolls Online"\live\AddOns\MasterWr
 mkdir c:\Users\ventr\Documents\"Elder Scrolls Online"\live\AddOns\MasterWritProcessing
 copy *.lua c:\Users\ventr\Documents\"Elder Scrolls Online"\live\AddOns\MasterWritProcessing
 copy *.xml c:\Users\ventr\Documents\"Elder Scrolls Online"\live\AddOns\MasterWritProcessing
+mkdir c:\Users\ventr\Documents\"Elder Scrolls Online"\live\AddOns\MasterWritProcessing\windows
+copy windows c:\Users\ventr\Documents\"Elder Scrolls Online"\live\AddOns\MasterWritProcessing\windows
 copy MasterWritProcessing.txt c:\Users\ventr\Documents\"Elder Scrolls Online"\live\AddOns\MasterWritProcessing
