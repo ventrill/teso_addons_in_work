546e3b7f2303a4b44fbf6301137d845e2e60c42e
Alex Vasilenko <vasilenko.a@cashberry.vn>
2024-12-30 18:52:59 +0200

    ~ Skill Rank Monitoring

--- SkillRankMonitoring/deploy.bat
+++ SkillRankMonitoring/deploy.bat
@@ -1,7 +1,9 @@
 rmdir /s /q c:\Users\ventr\Documents\"Elder Scrolls Online"\live\AddOns\SkillRankMonitoring
 mkdir c:\Users\ventr\Documents\"Elder Scrolls Online"\live\AddOns\SkillRankMonitoring
+mkdir c:\Users\ventr\Documents\"Elder Scrolls Online"\live\AddOns\SkillRankMonitoring\Classes
+mkdir c:\Users\ventr\Documents\"Elder Scrolls Online"\live\AddOns\SkillRankMonitoring\Windows
 copy *.lua c:\Users\ventr\Documents\"Elder Scrolls Online"\live\AddOns\SkillRankMonitoring
 copy *.xml c:\Users\ventr\Documents\"Elder Scrolls Online"\live\AddOns\SkillRankMonitoring
-mkdir c:\Users\ventr\Documents\"Elder Scrolls Online"\live\AddOns\SkillRankMonitoring\Classes
 copy Classes c:\Users\ventr\Documents\"Elder Scrolls Online"\live\AddOns\SkillRankMonitoring\Classes
+copy Windows c:\Users\ventr\Documents\"Elder Scrolls Online"\live\AddOns\SkillRankMonitoring\Windows
 copy SkillRankMonitoring.txt c:\Users\ventr\Documents\"Elder Scrolls Online"\live\AddOns\SkillRankMonitoring
